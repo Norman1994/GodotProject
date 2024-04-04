2b7749505b9750ab13866bae25f7a26d35edfd14
KazinDA <kazin-dima@mail.ru>
2024-04-04 11:53:52 +0300

    Реализовано главное меню

--- Scripts/node_2d_main.gd
+++ Scripts/node_2d_main.gd
@@ -3,7 +3,4 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	Dialogic.start("res://Timelines/timeline.dtl")
-	pass # Replace with function body.
-	
