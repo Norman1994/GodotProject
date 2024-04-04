180a6ae2bc4f1e1822334200e6c8bcd6e9f54bce
KazinDA <kazin-dima@mail.ru>
2024-04-04 14:01:38 +0300

    Добавлен новый тайлайн, новые звуки, кнопка выхода по нажатию esc

--- Scripts/node_2d_main.gd
+++ Scripts/node_2d_main.gd
@@ -4,3 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Dialogic.start("res://Timelines/timeline.dtl")
+
+func _input(event):
+	if event.is_action_pressed("ESC"):
+		get_tree().quit()
